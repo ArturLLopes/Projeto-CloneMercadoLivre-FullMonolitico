2660c1050b3e755bfd27ea87bdfa1a8ba9448f52
ArturLLopes <arturllopes@gmail.com>
2025-04-11 10:35:41 -0300

    feat: adiciona componente GaleriaProdutos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import BarraSuperior from "./widgets/BarraSuperiror";
 import MenuNavegacao from "./widgets/MenuNavegacao";
+import GaleriasProdutos from "./widgets/GaleriaProdutos";
 
 export default function Home() {
   return (
     <div>
       <header className="bg-amber-300">
-        <BarraSuperior/>
-        <MenuNavegacao/>
-
+        <BarraSuperior />
+        <MenuNavegacao />
       </header>
+
+      <main>
+        <GaleriasProdutos />
+      </main>
     </div>
   );
 }
- 
